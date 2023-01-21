13665dd944a3a52aaef9948e5de118a22d83add0
RajkumarJadhav <rajkumarjadhav0005@gmail.com>
2023-01-21 15:27:48 +0530

    updated project file

--- project_db.py
+++ project_db.py
@@ -11,13 +11,13 @@ db = mongo_db_client[database_name]
 
 collection_user = db['user_details']
 collection_token = db['token_details']
-
+########################################################################
 def register_user(request_data):
 
     collection_user.insert_one({'emailid': request_data['emailid'], 'password': request_data['password'],
                     'name':request_data['name'],"company":request_data['company']})
     return 'Success'
-
+#########################################################################
 def login_user(request_data):
 
     resp = collection_user.find_one({'emailid': request_data['emailid'], 'password': request_data['password']})
@@ -26,3 +26,4 @@ def login_user(request_data):
         return 'User not registered'
 
     return "Success"
+############################################################################
